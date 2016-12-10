898e14d932f3cf1a794d704dec273172098761c0
Kohányi Róbert <robert.kohanyi@codecool.com>
2016-12-10 21:51:01 +0000

    Advent of Code 3-b done

--- 03/main.py
+++ 03/main.py
@@ -1,9 +1,21 @@
 from sys import stdin
 
-count = 0
+
+table = []
 for line in stdin.readlines():
-    sides = [int(s) for s in line.split()]
-    sides.sort()
-    if sum(sides[0:2]) > sides[2]:
-        count += 1
+    row = [int(s) for s in line.split()]
+    table.append(row)
+
+count = 0
+for i in range(0, len(table), 3):
+    groupped_rows = []
+    for j in range(3):
+        groupped_rows.append(table[i + j])
+    for k in range(3):
+        sides = []
+        for j in range(3):
+            sides.append(groupped_rows[j][k])
+        sides.sort()
+        if sum(sides[0:2]) > sides[2]:
+            count += 1
 print(count)
