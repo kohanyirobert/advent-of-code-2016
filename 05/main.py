b3ec4431cdbdcce6757e7870b6ec556a24509e17
Kohányi Róbert <robert.kohanyi@codecool.com>
2016-12-11 10:31:37 +0000

    Advent of Code 5-b done

--- 05/main.py
+++ 05/main.py
@@ -3,12 +3,16 @@ from hashlib import md5
 
 door_id = stdin.read().strip('\n')
 
-password = []
+password_positions = {}
 i = 0
-while len(password) != 8:
+while len(password_positions) != 8:
     unhashed = door_id + str(i)
     hashed = md5(unhashed.encode()).hexdigest()
     if hashed[:5] == '00000':
-        password.append(hashed[5])
+        j = hashed[5]
+        if '0' <= j < '8' and j not in password_positions:
+            k = hashed[6]
+            password_positions[j] = k
     i += 1
-print(''.join(password))
+password = sorted(password_positions.items(), key=lambda x: x[0])
+print(''.join([x[1] for x in password]))
