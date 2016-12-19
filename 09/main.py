462be9f9442fb11c0846dd0fac0114a364bb1abf
Kohányi Róbert <robert.kohanyi@codecool.com>
2016-12-19 19:34:38 +0000

    Advent of Code 9-a done

---
+++ 09/main.py
@@ -0,0 +1,72 @@
+from sys import stdin
+
+
+class Marker:
+
+    @classmethod
+    def fromstring(cls, string):
+        start = string.find('(')
+        if start == -1:
+            return None
+        stop = string.find(')', start)
+        length, times = [int(_) for _ in string[start + 1:stop].split('x')]
+        return cls(string, start, stop, length, times)
+
+    def __init__(self, string, start, stop, length, times):
+        self.string = string
+        self.marker_start = start
+        self.marker_stop = stop
+        self.marker_length = length
+        self.marker_times = times
+        self.start = self.marker_stop + 1
+        self.stop = self.marker_stop + self.marker_length + 1
+
+    def __str__(self):
+        s = ', '.join(['{}={}'] * 6)
+        return s.format(
+            'marker_start', self.marker_start,
+            'marker_stop', self.marker_stop,
+            'marker_length', self.marker_length,
+            'marker_times', self.marker_times,
+            'start', self.start,
+            'stop', self.stop,
+        )
+
+    def decompress(self):
+        decompressed = self.string[:self.marker_start]
+        decompressed += self.marker_times * self.string[self.start:self.stop]
+        compressed = self.string[self.stop:]
+        return decompressed, compressed
+
+
+class Decompression:
+
+    def __init__(self, compressed):
+        self.finished = False
+        self.decompressed = ''
+        self.compressed = compressed
+
+    def is_decompressed(self):
+        return self.finished
+
+    def decompress(self):
+        marker = Marker.fromstring(self.compressed)
+        if marker == None:
+            self.finished = True
+            return
+        decompressed, compressed = marker.decompress()
+        self.decompressed += decompressed
+        self.compressed = compressed
+
+
+def get_compressed():
+    return stdin.read().strip()
+
+
+def main():
+    decompression = Decompression(get_compressed())
+    while not decompression.is_decompressed():
+        decompression.decompress()
+    print(len(decompression.decompressed))
+
+main()
